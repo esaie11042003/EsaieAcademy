1de06f39dbf0b5fef6458670c60c4a535a3ae912
esaie11042003 <esaieacademy@gmail.com>
2026-09-20 07:44:35 +0100

    Ajoute le champ role et autres infos au formulaire admin utilisateur

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,42 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin 
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
-admin.site.register(CustomUser, UserAdmin)
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "role",
+        "is_staff",
+        "is_superuser",
+        "actif",
+    )
+    list_filter = ("role", "is_staff", "is_superuser", "actif")
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Informations Esaïe Academy", {
+            "fields": (
+                "role",
+                "niveau_etude",
+                "sexe",
+                "date_naissance",
+                "adresse",
+                "phone",
+                "profile_picture",
+                "is_verified",
+                "must_change_password",
+                "actif",
+                "deleted",
+            )
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Informations Esaïe Academy", {
+            "fields": ("role",)
+        }),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
